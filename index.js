ab29e667c3deb7d9b2161c201beb05cd8e188ed1
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-03-19 12:01:15 +0100

    first comit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(`${chalk.green("Benoit")}`);
+console.log(`${chalk.yellow("Alexandre")}`);
+console.log(`${chalk.blue("Elias")}`);
+console.log(`${chalk.red("Vincent")}`);
